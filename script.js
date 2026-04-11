ad9f43a1914405d8862baa46b81626fe7c643477
Rayhan Ahmed <rmd09181@gmail.com>
2026-04-11 15:28:57 +0600

    add JavaScript to select DOM elements and hold weather API key

---
+++ script.js
@@ -0,0 +1,10 @@
+// select all the elements
+const cityInput = document.querySelector("#cityInput");
+const searchBtn = document.querySelector("#searchBtn");
+const cityText = document.querySelector("#cityName");
+const tempText = document.querySelector("#temperature");
+const weatherText = document.querySelector("#weather");
+
+// Hold weather API key
+const apiKey = "04b7afa2009cfdd91b44e48abf7e8b08";
+
