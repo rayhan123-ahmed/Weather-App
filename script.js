e2cb3de5c96b23a85e7a2c1fd5ab3ab48b08e83f
Rayhan Ahmed <rmd09181@gmail.com>
2026-04-11 15:29:18 +0600

    add function to fetch and display weather data based on city input

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ const weatherText = document.querySelector("#weather");
 // Hold weather API key
 const apiKey = "04b7afa2009cfdd91b44e48abf7e8b08";
 
+// creat a function to get weather
+
+async function getWeather(city) {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+  try {
+    const response = await fetch(URL);
+    const data = await response.json();
+
+    cityText.textContent = data.name;
+    tempText.textContent = data.main.temp + "°C";
+    weatherText.textContent = data.weather[0].main;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
